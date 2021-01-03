2106473f065235b426d57d6c76506fc22e46aada
MonitSharma <monitsharma437@gmail.com>
2021-01-03 09:35:17 +0530

    Command Line Utility

---
+++ eve/config.py
@@ -0,0 +1,7 @@
+WX_API_KEY = "d07f2aa07a97d2909da4dc70fd51d9bf"
+WX_LOCATION = "Palampur"
+WX_METRIC_TEMP = "celsius"
+WX_METRIC_WIND = "miles_hour"
+
+GITHUB_USER = 'MonitSharma'
+GITHUB_PASS = 'sh@rma244'
